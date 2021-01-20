37b97da225d84974d91bd9a160c3a6bc18640dc3
desrocs <52293553+desrocs@users.noreply.github.com>
2021-01-20 10:54:34 +0300

    Add project files.

---
+++ RefAndOutExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RefAndOutExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 30;
+            var sonuc = metod( ref num1, 51);
+            Console.WriteLine(sonuc);
+            Console.WriteLine(num1);
+
+           static int metod( ref int num1,int num2)
+            {
+                num1 = 111;
+                int sum = num1 + num2;
+                return sum;
+
+            }
+           
+
+            
+        }
+    }
+}
